99d89987e157fd2b2594057e1dd3f6034c415347
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2022-12-26 17:08:53 +0530

    Third Commit

--- backend/models/student.js
+++ backend/models/student.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
     name:{type:String,required:true},
-    age:{type:String,required:true},
+    age:{type:Number,required:true},
     gender:{type:String,required:true},
 })
 
--- backend/routes/students.js
+++ backend/routes/students.js
@@ -4,8 +4,19 @@ let Student = require("../models/student");
 router.route("/add").post((req,res)=>{
 
     const name = req.body.name;
-    const age = req.body.age;
+    const age = Number(req.body.age);
     const gender = req.body.gender;
 
+    const newStudent = new Student({
+        name,
+        age,
+        gender
+    })
+
+    newStudent.save()
+    .then(()=>res.json("Student Added"))
+    .catch(err=> res.status(400).json('Error:'+err));
 
 })
+
+module.exports=router;
