7a9267193f249b6dd7cacc9d8473d03eba327515
Pasindu-Madhuwantha <mpasindu72@gmail.com>
2022-12-29 17:25:04 +0530

    backend completed

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { response } = require("express");
 let Student = require("../models/student");
 
 
@@ -29,9 +30,47 @@ router.route("/add").post((req,res)=>{
 });
 
 
-router.route("/update/:id").put()
+router.route("/update/:id").put(async(req,res)=>{  //this is a diffrecnt method
+    let userId= req.params.id;
+    const{name,age,gender} = req.body;          // this called as D-Structure
 
+    const updateStudent ={
+        name,
+        age,
+        gender
+    }
 
+    const update = await Student.findByIdAndUpdate(userId,updateStudent).then(()=>{
+            res.status(200).send({status:"User updated"})
+ }).catch((err)=>{
+    console.log(err);
+    res.status(500).send({status:"Error with updating data"});
+ })
+});
+
+//delete
+
+router.route("/delete/:id").delete(async (req, res)=>{
+    let userId = req.params.id;
+
+    await Student.findByIdAndDelete(userId).then(()=>{
+        res.status(200).send({status:"User Deleted"});
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status:"Error with delete user",error:err.message});
+    })
+    
+})
+
+router.route("/get/:id").get(async (req,res)=>{
+    let userId = req.params.id;
+    const user= await Student.findById(userId).then((student)=>{
+        res.status(200).send({status:"User fetched",user:student})
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status:"Error with get user",error:err.message});
+    })
+})
 
 
 
